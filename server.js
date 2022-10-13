04d54489e489972bdea453148a9ceb428d3bc56a
Murilo Pacheco <murilo.pacheco99@gmail.com>
2022-10-13 03:55:18 -0300

    bug heroku

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000
-const path = require("path")
-app.use(express.static('build'))
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+// const PORT = process.env.PORT || 3000
+// const path = require("path")
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
-}
+app.use(express.static('build'))
+// app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "./src/index.tsx"));
+//   });
+// }
