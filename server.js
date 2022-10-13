e6bd4fe61ae0509246909c2798398485c74cc195
Murilo Pacheco <murilo.pacheco99@gmail.com>
2022-10-13 03:47:08 -0300

    bug heroku

--- server.js
+++ server.js
@@ -4,3 +4,10 @@ const PORT = process.env.PORT || 3000
 const path = require("path")
 app.use(express.static('build'))
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+}
