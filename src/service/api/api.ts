8a227859a3e7fe9aac55a35d8564b04bbb0060d1
Murilo Pacheco <murilo.pacheco99@gmail.com>
2023-04-10 17:17:40 -0300

    api planet scale

--- src/service/api/api.ts
+++ src/service/api/api.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "https://crud-login-api.herokuapp.com",
-  baseURL: "http://localhost:8080",
+  baseURL: "https://recadosapp.rj.r.appspot.com/",
 });
